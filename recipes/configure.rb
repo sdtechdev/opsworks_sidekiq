0bf7a0fa3f4fbd35f38e212785a456c123796b31
Frederico Araujo <fred.the.master@gmail.com>
2015-06-12 16:17:33 +0700

    Add back Memcached.yml support to deploy

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -36,4 +36,22 @@ node[:deploy].each do |application, deploy|
     end
   end
 
+  template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
+    source "memcached.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group deploy[:group]
+    owner deploy[:user]
+    variables(
+      memcached: deploy[:memcached] || {},
+      environment: deploy[:rails_env]
+    )
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      deploy[:memcached][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
 end
